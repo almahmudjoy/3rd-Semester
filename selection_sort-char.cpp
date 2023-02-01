280c3cc431bcb7b7a0111b61c8dd2881a35a4354
Abdullah Al Mahmud Joy <110623413+almahmudjoy@users.noreply.github.com>
2023-02-01 22:10:47 +0600

    Add files via upload

---
+++ selection_sort-char.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Minimum(char a[],int i,int n)
+
+{
+    int Min=a[i];
+    int l=i;
+    for(int j=i+1; j<=n; j++)
+    {
+        if(Min>a[j])
+        {
+            Min=a[j];
+
+            l=j;
+        }
+    }
+    return l;
+
+}
+
+void selection_sort(char a[], int n)
+{
+    for(int i=1; i<=n-1; i++)
+    {
+        int loc=Minimum(a,i,n);
+        {
+            int temp=a[loc];
+            a[loc]=a[i];
+            a[i]=temp;
+        }
+    }
+    cout<<"array :";
+    for(int i=1; i<=n; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
+}
+
+
+int main()
+{
+    cout<<"Enter array size : ";
+    int n;
+    cin>>n;
+    char a[n];
+    cout<<"Enter element : ";
+    for(int i=1; i<=n; i++)
+    {
+        cin>>a[i];
+    }
+    selection_sort(a,n);
+
+    // Minimum(a,n);
+}
+
+
